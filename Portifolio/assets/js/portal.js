fb9582dd8dd39a9f84c69b56a944cb45bf61f57b
Saulo Junior <36682208+SauloJuniorsj@users.noreply.github.com>
2021-03-03 00:08:51 -0300

    Delete Portifolio directory

--- Portifolio/assets/js/portal.js
+++
@@ -1,6 +0,0 @@
-var $input    = document.getElementById('input-file'),
-    $fileName = document.getElementById('file-name');
-
-$input.addEventListener('change', function(){
-$fileName.textContent = this.value;
-});
