1ae6751146c20ab89526072486912840a8d6c6de
MatheusNunes <53870054+sn-Matheus@users.noreply.github.com>
2021-05-10 23:46:13 -0300

    atualizado

--- app/Views/pages/moviegrid.php
+++ app/Views/pages/moviegrid.php
@@ -4,10 +4,10 @@
 			<div class="row">
 				<div class="col-md-12">
 					<div class="hero-ct">
-						<h1>Movie Listing - Grid Fullwidth</h1>
+						<h1>Os Melhores Filmes só Aqui.</h1>
 						<ul class="breadcumb">
-							<li class="active"><a href="#">Home</a></li>
-							<li> <span class="ion-ios-arrow-right"></span> movie listing</li>
+							<li class="active"><a href="#">Inicio</a></li>
+							<li> <span class="ion-ios-arrow-right"></span> Lista de Filmes</li>
 						</ul>
 					</div>
 				</div>
@@ -19,15 +19,15 @@
 			<div class="row">
 				<div class="col-md-12 col-sm-12 col-xs-12">
 					<div class="topbar-filter fw">
-						<p>Found <span>1,608 movies</span> in total</p>
-						<label>Sort by:</label>
+						<p>Encontrados <span>21 Filmes</span></p>
+						<label>Classificação :</label>
 						<select>
-							<option value="popularity">Popularity Descending</option>
-							<option value="popularity">Popularity Ascending</option>
-							<option value="rating">Rating Descending</option>
-							<option value="rating">Rating Ascending</option>
-							<option value="date">Release date Descending</option>
-							<option value="date">Release date Ascending</option>
+							<option value="popularity">Comédia</option>
+							<option value="popularity">Romance</option>
+							<option value="rating">Aventura</option>
+							<option value="rating">Ação</option>
+							<option value="date">Terror</option>
+							<option value="date">Suspense</option>
 						</select>
 						<a href="movielist.html" class="list"><i class="ion-ios-list-outline "></i></a>
 						<a href="moviegridfw.html" class="grid"><i class="ion-grid active"></i></a>
@@ -36,7 +36,7 @@
 						<div class="movie-item-style-2 movie-item-style-1">
 							<img src="/assets/images/uploads/mv1.jpg" alt="">
 							<div class="hvr-inner">
-								<a href="moviesingle.html"> Read more <i class="ion-android-arrow-dropright"></i> </a>
+								<a href="moviesingle.html"> Leia Mais <i class="ion-android-arrow-dropright"></i> </a>
 							</div>
 							<div class="mv-item-infor">
 								<h6><a href="#">oblivion</a></h6>
@@ -46,7 +46,7 @@
 						<div class="movie-item-style-2 movie-item-style-1">
 							<img src="/assets/images/uploads/mv2.jpg" alt="">
 							<div class="hvr-inner">
-								<a href="moviesingle.html"> Read more <i class="ion-android-arrow-dropright"></i> </a>
+								<a href="moviesingle.html"> Leia Mais <i class="ion-android-arrow-dropright"></i> </a>
 							</div>
 							<div class="mv-item-infor">
 								<h6><a href="#">into the wild</a></h6>
@@ -56,7 +56,7 @@
 						<div class="movie-item-style-2 movie-item-style-1">
 							<img src="/assets/images/uploads/mv-item3.jpg" alt="">
 							<div class="hvr-inner">
-								<a href="moviesingle.html"> Read more <i class="ion-android-arrow-dropright"></i> </a>
+								<a href="moviesingle.html"> Leia Mais <i class="ion-android-arrow-dropright"></i> </a>
 							</div>
 							<div class="mv-item-infor">
 								<h6><a href="#">Die hard</a></h6>
@@ -66,7 +66,7 @@
 						<div class="movie-item-style-2 movie-item-style-1">
 							<img src="/assets/images/uploads/mv-item4.jpg" alt="">
 							<div class="hvr-inner">
-								<a href="moviesingle.html"> Read more <i class="ion-android-arrow-dropright"></i> </a>
+								<a href="moviesingle.html"> Leia Mais <i class="ion-android-arrow-dropright"></i> </a>
 							</div>
 							<div class="mv-item-infor">
 								<h6><a href="#">The walk</a></h6>
@@ -76,7 +76,7 @@
 						<div class="movie-item-style-2 movie-item-style-1">
 							<img src="/assets/images/uploads/mv3.jpg" alt="">
 							<div class="hvr-inner">
-								<a href="moviesingle.html"> Read more <i class="ion-android-arrow-dropright"></i> </a>
+								<a href="moviesingle.html"> Leia Mais <i class="ion-android-arrow-dropright"></i> </a>
 							</div>
 							<div class="mv-item-infor">
 								<h6><a href="#">blade runner </a></h6>
@@ -86,7 +86,7 @@
 						<div class="movie-item-style-2 movie-item-style-1">
 							<img src="/assets/images/uploads/mv4.jpg" alt="">
 							<div class="hvr-inner">
-								<a href="moviesingle.html"> Read more <i class="ion-android-arrow-dropright"></i> </a>
+								<a href="moviesingle.html"> Leia Mais <i class="ion-android-arrow-dropright"></i> </a>
 							</div>
 							<div class="mv-item-infor">
 								<h6><a href="#">Mulholland pride</a></h6>
@@ -96,7 +96,7 @@
 						<div class="movie-item-style-2 movie-item-style-1">
 							<img src="/assets/images/uploads/mv5.jpg" alt="">
 							<div class="hvr-inner">
-								<a href="moviesingle.html"> Read more <i class="ion-android-arrow-dropright"></i> </a>
+								<a href="moviesingle.html"> Leia Mais <i class="ion-android-arrow-dropright"></i> </a>
 							</div>
 							<div class="mv-item-infor">
 								<h6><a href="#">skyfall: evil of boss</a></h6>
@@ -106,7 +106,7 @@
 						<div class="movie-item-style-2 movie-item-style-1">
 							<img src="/assets/images/uploads/mv-item1.jpg" alt="">
 							<div class="hvr-inner">
-								<a href="moviesingle.html"> Read more <i class="ion-android-arrow-dropright"></i> </a>
+								<a href="moviesingle.html"> Leia Mais <i class="ion-android-arrow-dropright"></i> </a>
 							</div>
 							<div class="mv-item-infor">
 								<h6><a href="#">Interstellar</a></h6>
@@ -117,7 +117,7 @@
 
 							<img src="/assets/images/uploads/mv-item2.jpg" alt="">
 							<div class="hvr-inner">
-								<a href="moviesingle.html"> Read more <i class="ion-android-arrow-dropright"></i> </a>
+								<a href="moviesingle.html"> Leia Mais <i class="ion-android-arrow-dropright"></i> </a>
 							</div>
 							<div class="mv-item-infor">
 								<h6><a href="#">The revenant</a></h6>
@@ -127,7 +127,7 @@
 						<div class="movie-item-style-2 movie-item-style-1">
 							<img src="/assets/images/uploads/mv-it10.jpg" alt="">
 							<div class="hvr-inner">
-								<a href="moviesingle.html"> Read more <i class="ion-android-arrow-dropright"></i> </a>
+								<a href="moviesingle.html"> Leia Mais <i class="ion-android-arrow-dropright"></i> </a>
 							</div>
 							<div class="mv-item-infor">
 								<h6><a href="#">harry potter</a></h6>
@@ -137,7 +137,7 @@
 						<div class="movie-item-style-2 movie-item-style-1">
 							<img src="/assets/images/uploads/mv-it11.jpg" alt="">
 							<div class="hvr-inner">
-								<a href="moviesingle.html"> Read more <i class="ion-android-arrow-dropright"></i> </a>
+								<a href="moviesingle.html"> Leia Mais <i class="ion-android-arrow-dropright"></i> </a>
 							</div>
 							<div class="mv-item-infor">
 								<h6><a href="#">guardians galaxy</a></h6>
@@ -147,7 +147,7 @@
 						<div class="movie-item-style-2 movie-item-style-1">
 							<img src="/assets/images/uploads/mv-it12.jpg" alt="">
 							<div class="hvr-inner">
-								<a href="moviesingle.html"> Read more <i class="ion-android-arrow-dropright"></i> </a>
+								<a href="moviesingle.html"> Leia Mais <i class="ion-android-arrow-dropright"></i> </a>
 							</div>
 							<div class="mv-item-infor">
 								<h6><a href="#">the godfather</a></h6>
@@ -157,7 +157,7 @@
 						<div class="movie-item-style-2 movie-item-style-1">
 							<img src="/assets/images/uploads/mv-item6.jpg" alt="">
 							<div class="hvr-inner">
-								<a href="moviesingle.html"> Read more <i class="ion-android-arrow-dropright"></i> </a>
+								<a href="moviesingle.html"> Leia Mais <i class="ion-android-arrow-dropright"></i> </a>
 							</div>
 							<div class="mv-item-infor">
 								<h6><a href="#">blue velvet</a></h6>
@@ -167,7 +167,7 @@
 						<div class="movie-item-style-2 movie-item-style-1">
 							<img src="/assets/images/uploads/mv-item7.jpg" alt="">
 							<div class="hvr-inner">
-								<a href="moviesingle.html"> Read more <i class="ion-android-arrow-dropright"></i> </a>
+								<a href="moviesingle.html"> Leia Mais <i class="ion-android-arrow-dropright"></i> </a>
 							</div>
 							<div class="mv-item-infor">
 								<h6><a href="#">gravity</a></h6>
@@ -177,7 +177,7 @@
 						<div class="movie-item-style-2 movie-item-style-1">
 							<img src="/assets/images/uploads/mv-item8.jpg" alt="">
 							<div class="hvr-inner">
-								<a href="moviesingle.html"> Read more <i class="ion-android-arrow-dropright"></i> </a>
+								<a href="moviesingle.html"> Leia Mais <i class="ion-android-arrow-dropright"></i> </a>
 							</div>
 							<div class="mv-item-infor">
 								<h6><a href="#">southpaw</a></h6>
@@ -187,7 +187,7 @@
 						<div class="movie-item-style-2 movie-item-style-1">
 							<img src="/assets/images/uploads/mv-it9.jpg" alt="">
 							<div class="hvr-inner">
-								<a href="moviesingle.html"> Read more <i class="ion-android-arrow-dropright"></i> </a>
+								<a href="moviesingle.html"> Leia Mais <i class="ion-android-arrow-dropright"></i> </a>
 							</div>
 							<div class="mv-item-infor">
 								<h6><a href="#">jurassic park</a></h6>
@@ -197,7 +197,7 @@
 						<div class="movie-item-style-2 movie-item-style-1">
 							<img src="/assets/images/uploads/mv-item9.jpg" alt="">
 							<div class="hvr-inner">
-								<a href="moviesingle.html"> Read more <i class="ion-android-arrow-dropright"></i> </a>
+								<a href="moviesingle.html"> Leia Mais <i class="ion-android-arrow-dropright"></i> </a>
 							</div>
 							<div class="mv-item-infor">
 								<h6><a href="#">the forest</a></h6>
@@ -207,7 +207,7 @@
 						<div class="movie-item-style-2 movie-item-style-1">
 							<img src="/assets/images/uploads/mv-item10.jpg" alt="">
 							<div class="hvr-inner">
-								<a href="moviesingle.html"> Read more <i class="ion-android-arrow-dropright"></i> </a>
+								<a href="moviesingle.html"> Leia Mais <i class="ion-android-arrow-dropright"></i> </a>
 							</div>
 							<div class="mv-item-infor">
 								<h6><a href="#">spectre</a></h6>
@@ -217,7 +217,7 @@
 						<div class="movie-item-style-2 movie-item-style-1">
 							<img src="/assets/images/uploads/mv-item11.jpg" alt="">
 							<div class="hvr-inner">
-								<a href="moviesingle.html"> Read more <i class="ion-android-arrow-dropright"></i> </a>
+								<a href="moviesingle.html"> Leia Mais <i class="ion-android-arrow-dropright"></i> </a>
 							</div>
 							<div class="mv-item-infor">
 								<h6><a href="#">strager things</a></h6>
@@ -227,7 +227,7 @@
 						<div class="movie-item-style-2 movie-item-style-1">
 							<img src="/assets/images/uploads/mv-item12.jpg" alt="">
 							<div class="hvr-inner">
-								<a href="moviesingle.html"> Read more <i class="ion-android-arrow-dropright"></i> </a>
+								<a href="moviesingle.html"> Leia Mais <i class="ion-android-arrow-dropright"></i> </a>
 							</div>
 							<div class="mv-item-infor">
 								<h6><a href="#">la la land</a></h6>
@@ -237,41 +237,29 @@
 						<div class="movie-item-style-2 movie-item-style-1">
 							<img src="/assets/images/uploads/mv1.jpg" alt="">
 							<div class="hvr-inner">
-								<a href="moviesingle.html"> Read more <i class="ion-android-arrow-dropright"></i> </a>
+								<a href="moviesingle.html"> Leia Mais <i class="ion-android-arrow-dropright"></i> </a>
 							</div>
 							<div class="mv-item-infor">
 								<h6><a href="#">oblivion</a></h6>
 								<p class="rate"><i class="ion-android-star"></i><span>8.1</span> /10</p>
 							</div>
 						</div>
-						<div class="movie-item-style-2 movie-item-style-1">
-							<img src="/assets/images/uploads/mv2.jpg" alt="">
-							<div class="hvr-inner">
-								<a href="moviesingle.html"> Read more <i class="ion-android-arrow-dropright"></i> </a>
-							</div>
-							<div class="mv-item-infor">
-								<h6><a href="#">into the wild</a></h6>
-								<p class="rate"><i class="ion-android-star"></i><span>7.8</span> /10</p>
-							</div>
-						</div>
 
 					</div>
 					<div class="topbar-filter">
-						<label>Movies per page:</label>
+						<label>Próximo Filme:</label>
 						<select>
-							<option value="range">20 Movies</option>
-							<option value="saab">10 Movies</option>
+							<option value="range">20 Filmes</option>
+							<option value="saab">10 Filmes</option>
 						</select>
 
 						<div class="pagination2">
-							<span>Page 1 of 2:</span>
+							<span>Página 1 de 5:</span>
 							<a class="active" href="#">1</a>
 							<a href="#">2</a>
 							<a href="#">3</a>
-							<a href="#">...</a>
-							<a href="#">78</a>
-							<a href="#">79</a>
-							<a href="#"><i class="ion-arrow-right-b"></i></a>
+							<a href="#">4</a>
+							<a href="#">5</a>
 						</div>
 					</div>
 				</div>
